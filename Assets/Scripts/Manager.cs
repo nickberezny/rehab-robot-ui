3bf642eb6d9b68ac7fce0dae2c3f2960755b6bba
Nick <nickberezny@hotmail.com>
2022-04-09 09:07:53 -0400

    added unity project files

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    public enum states { Init, WaitForConn, InitDevice, Running, Stop, Shutdown  }
+    public states state = states.Init;
+
+    public ConnectionMenu connectionMenu;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public IEnumerator RecieveMessage(string msg)
+    {
+        //
+        switch(state)
+        {
+            case states.WaitForConn:
+                WaitForConnMessageHandler(msg);
+                break;
+            default:
+                break;
+        }
+        yield return null;
+    }
+
+    private void WaitForConnMessageHandler(string msg)
+    {
+        switch(msg)
+        {
+            case "ROBOT":
+                Debug.Log("robot!");
+                connectionMenu.changeButtonInteractable(msg, true);
+                break;
+            case "OPENCV":
+                connectionMenu.changeButtonInteractable(msg, true);
+                break;
+            default:
+                break;
+        }
+    }
+
+    //connect to server
+    //wait for available devices
+    //enable select buttons 
+
+}
---
+++ Assets/Scripts/Menu/ConnectionMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConnectionMenu : MonoBehaviour
+{
+    [SerializeField] Button _robot;
+    [SerializeField] Button _openCV;
+
+    private void Awake()
+    {
+        _robot.interactable = false;
+        _openCV.interactable = false;
+    }
+    private void Start()
+    {
+        FindObjectOfType<Manager>().connectionMenu = this;
+    }
+    public void changeButtonInteractable(string buttonName, bool set)
+    {
+        if (buttonName == "ROBOT") _robot.interactable = set;
+        else if (buttonName == "OPENCV") _openCV.interactable = set;
+
+    }
+}
